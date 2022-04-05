09b3e8fe5b4452da152879dcf2d8d9736b0b32a4
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-05 12:52:11 -0300

    style: identa arquivo de desafio3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,1 +1,6 @@
-db.produtos.find({}, { _id: 0, nome: 1, vendidos: 1 }).sort({ vendidos: -1 }).limit(1);
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, vendidos: 1 },
+).sort({ vendidos: -1 }).limit(1);
+
+// https://stackoverflow.com/questions/32076382/mongodb-how-to-get-max-value-from-collections

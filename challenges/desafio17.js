5c49dd6b316fc33f4aab990cbc3f0db5fbd9303b
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-05 17:49:14 -0300

    feat: insere nova coleção dentro de eccomerce

---
+++ challenges/desafio17.js
@@ -0,0 +1,11 @@
+db.createCollection("resumoProdutos");
+
+db.resumoProdutos.insertOne({
+  franquia: "McDonalds",
+  totalProdutos: 5,
+});
+
+db.resumoProdutos.find(
+  { franquia: "McDonalds" },
+  { _id: 0, franquia: 1, totalProdutos: 1 },
+);
